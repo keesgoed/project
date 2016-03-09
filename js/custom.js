7dbbfec1cafd9b067cafdc7a364b76147551ea39
ritchiebos <richard.bos93@gmail.com>
2016-03-09 18:16:47 +0100

    Add Datatables CSS / Change custom.js + custom.css

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,26 @@
 $(document).ready(function(){
-  var table = $('.table').DataTable();
-  var tt = new $.fn.dataTable.TableTools( table );
-
-  $( tt.fnContainer() ).insertBefore('div.dataTables_wrapper');
+  var str = window.location.pathname; // Returns path only
+  var webpages = str.substring(14, 28);
+  
+  switch(webpages) {
+    case "klanten":
+        initDTTT();
+        break;
+    case "offertes":
+        console.log(webpages);
+        break;
+    case "facturatie":
+        console.log(webpages);
+        break; 
+  }
 });
+
+function initDTTT() {
+  var table = $('.table').DataTable();
+  var tt = new $.fn.dataTable.TableTools(table, {
+    "sSwfPath": "http://cdn.datatables.net/tabletools/2.2.2/swf/copy_csv_xls_pdf.swf",
+    "aButtons": ["xls", "pdf", "print"]
+  });
+  
+  $(tt.fnContainer()).insertBefore('div.dataTables_wrapper');
+}
