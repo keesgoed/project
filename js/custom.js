4e41c5e8f2ca13f1ab9adcd31e6a14497027060a
Kees Goedegebuure <keesgoed@hotmail.com>
2016-03-14 11:50:43 +0100

    custom.js copy

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,28 @@
 $(document).ready(function(){
-  var table = $('.table').DataTable();
-  var tt = new $.fn.dataTable.TableTools( table );
+  var pathname = window.location.pathname; // Returns path only
+  var paths = pathname.split("/");
+  var webpages = paths[2].substring(0, 20);
 
-  $( tt.fnContainer() ).insertBefore('div.dataTables_wrapper');
-
+  switch(webpages) {
+    case "klanten":
+      initDTTT(webpages);
+      break;
+    case "offertes":
+      console.log(webpages);
+      break;
+    case "facturatie":
+      console.log(webpages);
+      break;
+  }
 });
+
+function initDTTT(webpage) {
+  var table = $("#table-" + webpage).DataTable();
+  var tt = new $.fn.dataTable.TableTools(table, {
+    "sSwfPath": "//cdn.datatables.net/tabletools/2.2.2/swf/copy_csv_xls_pdf.swf",
+    "aButtons": ["xls", "pdf", "print"]
+  });
+
+  $(tt.fnContainer()).insertBefore("div.dataTables_wrapper");
+}
+
