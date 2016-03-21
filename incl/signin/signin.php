963cdc80eab4b3f73f8e8559cd50f78345c5fec5
Kees Goedegebuure <keesgoed@hotmail.com>
2016-03-21 11:17:50 +0100

    Fix db

--- incl/signin/signin.php
+++ incl/signin/signin.php
@@ -3,19 +3,20 @@
 class Signin extends Database {
   public $username = "Gebruikersnaam";
   public $password = "Wachtwoord";
-  
+
   private $usr;
   private $pwd;
   private $sql;
   private $query;
   private $rows;
+  private $accounts;
   private $row;
-  
+
   public function __construct() {
     if(isset($_POST['signin'])) {
       $this->usrError();
       $this->pwdError();
-      
+
       if(!empty($_POST['username']) and !empty($_POST['password'])) {
         $this->connDatabase();
         $this->dbError();
@@ -26,52 +27,52 @@ class Signin extends Database {
       }
     }
   }
-  
+
   public function usrError() {
     if(empty($_POST['username'])) {
       $this->username = "Vul uw gebruiksersnaam in";
     }
   }
-  
+
   public function pwdError() {
     if(empty($_POST['password'])) {
       $this->password = "Vul uw wachtwoord in";
     }
   }
-  
+
   public function escString() {
     $this->usr = $_POST['username'];
     $this->pwd = $_POST['password'];
-    
+
     $this->usr = mysqli_real_escape_string($this->db, $this->usr);
     $this->pwd = mysqli_real_escape_string($this->db, $this->pwd);
   }
-  
+
   public function qryDatabase() {
     $this->sql = "
-      SELECT a.account_id, a.username, a.password
+      SELECT a.accounts_id, a.username, a.password
 			FROM accounts AS a
 			WHERE a.username = '".$this->usr."'
 				AND a.password = '".$this->pwd."'
     ";
-    
+
     $this->query = mysqli_query($this->db, $this->sql);
-    
-    while ($this->rows = mysqli_fetch_assoc($this->query)) {
+
+    while($this->rows = mysqli_fetch_assoc($this->query)) {
       $this->accounts = $this->rows;
     }
-    
+
     $this->row = mysqli_num_rows($this->query);
   }
-  
+
   public function initSession() {
     if($this->row == 1) {
       $_SESSION['signedin'] = $this->accounts;
-      
+
       header("Location: ".$_SERVER['REQUEST_URI']);
     }
   }
-  
+
   public function rowError() {
     if($this->row == 0) {
       $this->username = "Gebruikersnaam is verkeerd";
